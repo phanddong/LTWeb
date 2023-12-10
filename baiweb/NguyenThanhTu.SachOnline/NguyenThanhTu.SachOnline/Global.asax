5f724f1afbf537b4093d2554f95deb333121bcba
John Doe <2124802010522@student.tdmu.edu.vn>
2023-12-10 23:59:41 +0700

    first commit

---
+++ baiweb/NguyenThanhTu.SachOnline/NguyenThanhTu.SachOnline/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PDD.SachOnline.MvcApplication" Language="C#" %>
---
+++ baiweb/NguyenThanhTu.SachOnline/NguyenThanhTu.SachOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PDD.SachOnline.Startup))]
+namespace PDD.SachOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
